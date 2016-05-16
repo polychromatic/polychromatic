d1c03a1f5eb95b0b342201769727c5cc94854a83
Luke Horwell <lukehorwell37+code@gmail.com>
2016-05-16 18:22:17 +0100

    Update project name.

--- controller.py
+++ controller.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 #
-# Chroma Config Tool is free software: you can redistribute it and/or modify
+# Polychromatic Controller is free software: you can redistribute it and/or modify
 # it under the temms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Chroma Config Tool is distributed in the hope that it will be useful,
+# Polychromatic Controller is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Chroma Config Tool. If not, see <http://www.gnu.org/licenses/>.
+# along with the software. If not, see <http://www.gnu.org/licenses/>.
 #
 # Copyright (C) 2015-2016 Luke Horwell <lukehorwell37+code@gmail.com>
 #               2015-2016 Terry Cain <terry@terrys-home.co.uk>
