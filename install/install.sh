1f26b6da21edea719ec117dc3e065251b61ce1b0
Luke Horwell <lukehorwell37+code@gmail.com>
2016-06-20 12:04:37 +0100

    install.sh: Print pretty colours and use system's Python path.
    * Prints pretty colours.
    * Uses dynamic /usr/share/applications path.
    * Detect Python3 modules path. (Good for arch users!)

--- install/install.sh
+++ install/install.sh
@@ -24,10 +24,26 @@
 target_data="/usr/share/polychromatic"
 target_bin="/usr/bin"
 target_icon="/usr/share/icons"
-modules="/usr/lib/python3/dist-packages/polychromatic"
+target_apps="/usr/share/applications"
+python_path=$(python3 -c "import sys; print(sys.path[-1])")
+polyc_modules="$python_path/polychromatic"
+razer_modules="$python_path/razer/"
 locale_dir="/usr/share/locale/"
 source=$(dirname "$0")/..
 dependencies_apt="gir1.2-webkit2-4.0 python3-gi gir1.2-appindicator3-0.1"
+
+# Pretty colours!
+function printg() {
+    echo -e "\033[92m$*\033[0m"
+}
+
+function printr() {
+    echo -e "\033[91m$*\033[0m"
+}
+
+function printy() {
+    echo -e "\033[93m$*\033[0m"
+}
 
 # Are we root?
 if [ "$(id -u)" != "0" ]; then
@@ -38,7 +54,7 @@ fi
 
 # Check for existing installation.
 if [ -d "$target_data" ]; then
-    echo "An installation already exists. Removing first..."
+    printy "An installation already exists. Removing first..."
     $(dirname "$0")/uninstall.sh
     if [ -d "$target_data" ]; then
         echo "Uninstall failed. Aborting."
@@ -54,70 +70,78 @@ function get_distro() {
 
 distro=`get_distro`
 if [ "$distro" == "debian" ] || [ "$distro" == "ubuntu" ]; then
-    echo -e "Installing dependencies...\n"
+    printg "Installing dependencies...\n"
     sudo apt install $dependencies_apt
     echo -e ''
 else
-    echo "*************************"
-    echo "Dependencies cannot be automatically installed on this distro."
+    echo "**************************************************"
+    printy "Dependencies cannot be automatically installed on this distro."
     echo "Please see the README before using the application."
-    echo "*************************"
+    echo "**************************************************"
 fi
 
 # Check if the Razer Python modules are present.
-echo "Checking if Razer modules are present..."
-razer_path='/usr/lib/python3/dist-packages/razer/'
-if [ ! -d "$razer_path" ]; then
-    echo "*************************"
-    echo "The razer-chroma-drivers Python modules are missing!"
-    echo "Please install them from http://pez2001.github.io/razer_chroma_drivers/"
-    echo -e "It could be possible that they are in a different path to '$razer_path'"
-    echo "*************************"
+printy "Checking if Razer modules are present..."
+if [ ! -d "$razer_modules" ]; then
+    echo "**************************************************"
+    printr "The razer-chroma-drivers Python modules are missing!"
+    echo -e "Please install them from \033[93mhttp://pez2001.github.io/razer_chroma_drivers/\033[0m"
+    echo -e "They are expected to be installed here: \033[93m$razer_modules\033[0m"
+    echo "**************************************************"
     read -p "Continue anyway? [y] " action
     if [ ! "$action" == 'y' ]; then
-        echo "Install Aborted."
+        printr "Polychromatic was not installed."
         exit 1
     fi
 fi
 
 # Create directories
-echo "Copying files..."
+printg "Creating new directories..."
 mkdir "$target_data"
-mkdir "$modules"
+mkdir "$polyc_modules"
 
 # Copy bin files.
+printg "Copying to $target_bin..."
 cp "$source/polychromatic-controller" "$target_bin/"
 cp "$source/polychromatic-tray-applet" "$target_bin/"
 chmod +x "$target_bin/polychromatic-controller"
 chmod +x "$target_bin/polychromatic-tray-applet"
 
 # Copy data files.
+printg "Copying to $target_data..."
 cp -r "$source/data/"* "$target_data/"
 
 # Copy Python modules
-cp -r "$source/pylib/"* "$modules/"
+printg "Copying to $polyc_modules..."
+cp -r "$source/pylib/"* "$polyc_modules/"
 
 # Copy icons
+printg "Copying to $target_icon..."
 cp "$source/install/hicolor/scalable/apps/polychromatic.svg" "$target_icon/hicolor/scalable/apps/polychromatic.svg"
 
 # Copy locales
+printg "Copying to $locale_dir..."
 rsync -rlpt --exclude="polychromatic-controller.pot" --exclude="polychromatic-tray-applet.pot" --exclude=*.po "$source/locale/" "$locale_dir"
 
 # Copy desktop launchers
-cp "$source/install/polychromatic-controller.desktop" /usr/share/applications/
-cp "$source/install/polychromatic-tray.desktop" /usr/share/applications/
+printg "Copying to $target_apps..."
+cp "$source/install/polychromatic-controller.desktop" "$target_apps/"
+cp "$source/install/polychromatic-tray.desktop" "$target_apps/"
 
 # Keep a copy of the uninstall script for manual removal later.
 cp "$source/install/uninstall.sh" "$target_data/uninstall-polychromatic.sh"
 
 # Post installation
-echo "Updating icon cache..."
+printg "Updating icon cache..."
 update-icon-caches /usr/share/icons/hicolor/
 
 # Success!
-echo "Installation Success!"
+printg "\nPolychromatic Installed!"
 echo "----------------------------"
 echo "To manually remove the software from your system cleanly later, run:"
-echo "$target_data/uninstall-polychromatic.sh"
+printy "$target_data/uninstall-polychromatic.sh"
+echo "----------------------------"
+echo "To manually update, run:"
+printy "$(pwd)/update.sh"
+echo -e "----------------------------"
 exit 0
-
