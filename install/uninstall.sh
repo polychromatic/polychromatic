e9e38e1d2a0600c6c758f78ec9c31dfd643e884d
Luke Horwell <lukehorwell37+code@gmail.com>
2016-08-14 22:26:07 +0100

    Rename tray applet desktop files for consistency.

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -50,7 +50,7 @@ rm     "$target_bin/polychromatic-controller"
 rm     "$target_bin/polychromatic-tray-applet"
 rm     "$target_icon/hicolor/scalable/apps/polychromatic.svg"
 rm -rf /usr/share/applications/polychromatic-controller.desktop
-rm -rf /usr/share/applications/polychromatic-tray.desktop
+rm -rf /usr/share/applications/polychromatic-tray-applet.desktop
 rm -rf /etc/xdg/autostart/polychromatic-tray-applet.desktop
 
 # Delete locales
