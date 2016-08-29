791fe92a1b5759476fa56ced53f384782643130b
Luke Horwell <lukehorwell37+code@gmail.com>
2016-08-29 17:36:17 +0100

    Drop (now obsolete) scripts.

--- scripts/razer/set_game_mode.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#
-#  Example script to turn on or off game mode.
-#
-
-import razer.daemon_dbus
-daemon = razer.daemon_dbus.DaemonInterface()
-daemon.set_game_mode(True)
--- scripts/razer/set_macro.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#
-#  Example script to enable the macro keys.
-#
-
-import razer.daemon_dbus
-daemon = razer.daemon_dbus.DaemonInterface()
-daemon.marco_keys(True)
