150799b2a522104057adff9e7bb28b6246f7772f
Luke Horwell <luke@ubuntu-mate.org>
2017-07-22 22:31:02 +0100

    Make "green" string translatable.

--- pylib/common.py
+++ pylib/common.py
@@ -358,15 +358,15 @@ def get_green_shades():
     like the Razer BlackWidow Ultimate.
     """
     return {
-    "1": {"name": "Green 1", "col": [0, 255, 0]},
-    "2": {"name": "Green 2", "col": [0, 225, 0]},
-    "3": {"name": "Green 3", "col": [0, 200, 0]},
-    "4": {"name": "Green 4", "col": [0, 175, 0]},
-    "5": {"name": "Green 5", "col": [0, 150, 0]},
-    "6": {"name": "Green 6", "col": [0, 125, 0]},
-    "7": {"name": "Green 7", "col": [0, 100, 0]},
-    "8": {"name": "Green 8", "col": [0, 75, 0]},
-    "9": {"name": "Green 9", "col": [0, 50, 0]},
+        "1": {"name": _("Green") + " 1", "col": [0, 255, 0]},
+        "2": {"name": _("Green") + " 2", "col": [0, 225, 0]},
+        "3": {"name": _("Green") + " 3", "col": [0, 200, 0]},
+        "4": {"name": _("Green") + " 4", "col": [0, 175, 0]},
+        "5": {"name": _("Green") + " 5", "col": [0, 150, 0]},
+        "6": {"name": _("Green") + " 6", "col": [0, 125, 0]},
+        "7": {"name": _("Green") + " 7", "col": [0, 100, 0]},
+        "8": {"name": _("Green") + " 8", "col": [0, 75, 0]},
+        "9": {"name": _("Green") + " 9", "col": [0, 50, 0]},
     }
 
 
