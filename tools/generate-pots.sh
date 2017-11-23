64b1eb2909f083992887dd613016de4896255fbd
Luke Horwell <luke@ubuntu-mate.org>
2017-11-23 07:26:14 +0000

    Update locations for sourcing translations.

--- tools/generate-pots.sh
+++ tools/generate-pots.sh
@@ -7,9 +7,15 @@ cd "$(dirname $0)"/../
 
 echo "Generating pots..."
 
-pygettext -d polychromatic-controller   polychromatic-controller
-pygettext -d polychromatic-tray-applet  polychromatic-tray-applet
-pygettext -d polychromatic-common       pylib/common.py
+pygettext -d polychromatic-controller       polychromatic-controller
+pygettext -d polychromatic-tray-applet      polychromatic-tray-applet
+pygettext -d polychromatic-common           pylib/common.py
+pygettext -d polychromatic-page-error       pylib/screens/error.py
+pygettext -d polychromatic-page-loading     pylib/screens/loading.py
+pygettext -d polychromatic-page-devices     pylib/screens/main/devices.py
+pygettext -d polychromatic-page-preferences pylib/screens/main/preferences.py
+pygettext -d polychromatic-page-profiles    pylib/screens/main/profiles.py
+
 for file in $(ls pylib/screens/*.py); do
     if [ "$(basename "$file")" == "__init__.py" ]; then
         continue
