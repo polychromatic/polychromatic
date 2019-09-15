56dbadd1f891bc72e8ccfa48c4d784c40240cddc
Luke Horwell <code@horwell.me>
2019-09-15 18:50:47 +0100

    Support 'sass' and 'sassc' for compiling styling.

--- source/build-sass.sh
+++ source/build-sass.sh
@@ -2,14 +2,28 @@
 #
 # Builds the styling for the controller application.
 #
-
-cmd=$(which sass)
-if [ -z "$cmd" ]; then
-    echo "Please install the command for 'sass' and try again."
-    exit 1
-fi
+# Supported implementations:
+#   - SASSC (sassc)
+#   - Dart SASS (sass)
+#
 
 cd $(dirname "$0")/sass/
 
-sass ./controller.scss ../../data/ui/controller.css --style=compressed --no-source-map
+# Find an implementation of SASS to use.
+sassc=$(which sassc 2>/dev/null)
+sass=$(which sass 2>/dev/null)
 
+if [ -z "$sass" ] && [ -z "$sassc" ]; then
+    echo "Please install a package that provides 'sassc' or 'sass' and try again."
+    echo "Try: sassc (from your distro's repositories); sass (npm) or Dart SASS."
+    exit 1
+fi
+
+if [ ! -z "$sassc" ]; then
+    echo "Compiling styling... (sassc)"
+    sassc ./controller.scss ../../data/ui/controller.css --sass --style compressed
+
+elif [ ! -z "$sass" ]; then
+    echo "Compiling styling... (sass)"
+    sass ./controller.scss ../../data/ui/controller.css --style=compressed --no-source-map
+fi
