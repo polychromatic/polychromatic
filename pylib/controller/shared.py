8474d1d328ef05c2d6ffefafbef54b6e216e32a9
Luke Horwell <code@horwell.me>
2020-07-15 17:52:16 +0100

    Controller: Embed 'Play' font (again)
    Many non-Debian distros don't actually have a working package.
    "ttf-play" on the AUR isn't actually installable.
    
    The font is only used when the Polychromatic Qt style is used.

--- pylib/controller/shared.py
+++ pylib/controller/shared.py
@@ -12,9 +12,8 @@ from .. import locales
 from .. import preferences as pref
 
 import os
-from PyQt5 import uic
 from PyQt5.QtCore import QThread, QRunnable, QThreadPool, pyqtSlot
-from PyQt5.QtGui import QIcon, QPalette, QColor
+from PyQt5.QtGui import QIcon, QPalette, QColor, QFont, QFontDatabase
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QToolButton, QMessageBox, QListWidget
 
@@ -27,27 +26,36 @@ def load_qt_theme(app, qapp, main_window):
         qapp            QApplication() object
         main_window     QMainWindow() object
     """
-    if app.custom_qt_theme:
-        qapp.setStyle("Fusion")
+    if app.system_qt_theme:
+        return
 
-        ui_palette = QPalette()
-        black = QColor(0, 0, 0)
-        white = QColor(255, 255, 255)
-        primary = QColor(0, 255, 0)
-        ui_palette.setColor(QPalette.Window, QColor(0, 0, 0))
-        ui_palette.setColor(QPalette.WindowText, white)
-        ui_palette.setColor(QPalette.Base, QColor(25, 25, 25))
-        ui_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-        ui_palette.setColor(QPalette.ToolTipBase, white)
-        ui_palette.setColor(QPalette.ToolTipText, white)
-        ui_palette.setColor(QPalette.Text, white)
-        ui_palette.setColor(QPalette.Button, QColor(50, 50, 50)) #323232
-        ui_palette.setColor(QPalette.ButtonText, white)
-        ui_palette.setColor(QPalette.Link, primary)
-        ui_palette.setColor(QPalette.Highlight, primary)
-        ui_palette.setColor(QPalette.HighlightedText, black)
-        main_window.setPalette(ui_palette)
+    # Load "Play" font
+    QFontDatabase.addApplicationFont(os.path.join(app.data_path, "qt", "fonts", "Play_regular.ttf"))
+    font = QFont("Play", 10, 1)
+    qapp.setFont(font)
+    main_window.menuBar.setFont(font)
+    main_window.findChild(QWidget, "MainTabCustom").setFont(font)
 
-        with open(os.path.join(app.data_path, "qt", "style.qss"), "r") as f:
-            main_window.setStyleSheet(f.read().replace("[data]", app.data_path))
+    # Load basic colour palettes
+    ui_palette = QPalette()
+    black = QColor(0, 0, 0)
+    white = QColor(255, 255, 255)
+    primary = QColor(0, 255, 0)
+    secondary = QColor(0, 128, 0)
+    ui_palette.setColor(QPalette.Window, QColor(0, 0, 0))
+    ui_palette.setColor(QPalette.WindowText, white)
+    ui_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    ui_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    ui_palette.setColor(QPalette.ToolTipBase, white)
+    ui_palette.setColor(QPalette.ToolTipText, white)
+    ui_palette.setColor(QPalette.Text, white)
+    ui_palette.setColor(QPalette.Button, QColor(50, 50, 50)) #323232
+    ui_palette.setColor(QPalette.ButtonText, white)
+    ui_palette.setColor(QPalette.Link, primary)
+    ui_palette.setColor(QPalette.Highlight, secondary)
+    ui_palette.setColor(QPalette.HighlightedText, white)
+    main_window.setPalette(ui_palette)
 
+    # Load QSS (essentially CSS) with Polychromatic's design
+    with open(os.path.join(app.data_path, "qt", "style.qss"), "r") as f:
+        main_window.setStyleSheet(f.read().replace("[data]", app.data_path))
