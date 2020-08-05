polychromatic/polychromatic

commit cf0aadbe9843f2f35204cf72645ca9f096711d18

pref: Try determining a themed default tray icon

pylib/common.py
@@ -157,7 +157,7 @@ def set_default_tray_icon(pref):
     elif theme_env.startswith("Ambiant") or theme_env.startswith("Ambiance"):
         icon_value = "ui/img/tray/light/humanity.svg"
 
-    pref.set("tray", "icon", icon_value)
+    return icon_value
 
 
 def get_tray_icon(dbg, icon_value):

pylib/preferences.py
@@ -98,7 +98,7 @@ def load_file(filepath):
         _validate("controller", "show_menu_bar", bool, True)
         _validate("controller", "system_qt_theme", bool, False)
         _validate("tray", "mode", int, 0)
-        _validate("tray", "icon", str, "ui/img/tray/light/polychromatic.svg")
+        _validate("tray", "icon", str, common.get_default_tray_icon())
 
     return(data)
 

commit a85d96c780e869768ead179333e2f92fb0984361

Commit missing rename get_default_tray_icon()
Regressed from cf0aadbe9843f2f35204cf72645ca9f096711d18

pylib/common.py
@@ -137,7 +137,7 @@ def get_green_shades():
     return colours
 
 
-def set_default_tray_icon(pref):
+def get_default_tray_icon():
     """
     Determines which tray icon is best suited for the current desktop environment
     or theme.