de33a806e902e623b540cd3139e14f22a21b4c0e
Luke Horwell <code@horwell.me>
2020-12-28 17:41:30 +0000

    fx: Use 'colours' library to process colours
    Adds a dependency on python-colours, available from most distribution
    repositories.

--- pylib/fx.py
+++ pylib/fx.py
@@ -9,7 +9,15 @@ connects to the supported backend, regardless of driver.
 
 This is used internally for rendering software effects as well as "scripted"
 effects written by users.
+
+To achieve effects, colours may be converted between different colour spaces:
+    - RGB (Red, Green, Blue)            -> using hex codes for storage
+    - HSV (Hue, Saturation, Value)      -> for colour intensity
+    - HSL (Hue, Saturation, Lightness)  -> for black/white intensity
 """
+
+import colour
+import math
 
 from . import common
 
@@ -22,7 +30,7 @@ class FX(object):
     This object is the 'fx' object used in custom effect scripts as well as
     Polychromatic internally for software effects.
 
-    All classes should be implemented. If they are unused
+    All classes stubbed as NotImplementedError should be implemented.
     """
     def __init__(self, rows, cols, name, backend, form_factor, serial):
         """
@@ -42,24 +50,9 @@ class FX(object):
         self.backend = backend
         self.form_factor = form_factor
 
-    def rgb_to_hex(self, red, green, blue):
-        """
-        Converts a RGB value to a hex colour string.
-
-        Input:  (0, 255, 0)
-        Output: "#00FF00"
-        """
-        return common.rgb_to_hex([red, green, blue])
-
-    def hex_to_rgb(self, value):
-        """
-        Converts a HEX colour string to a RGB string.
-
-        Input:  "#FF0000"
-        Output: [255, 0, 0]
-        """
-        return common.hex_to_rgb(value)
-
+    #######################################################
+    # To be implemented by the backend
+    #######################################################
     def set(self, x, y, red, green, blue):
         """
         Dummy implementation for the backend to set an LED position to the
@@ -88,8 +81,148 @@ class FX(object):
     def brightness(self, percent):
         """
         Dummy implementation for the backend to set the brightness of all the
-        LEDs. Could be used for fade effects.
+        LEDs. Could be used for fade effects globally.
 
         Input: percent (int between 0-100)
         """
         raise NotImplementedError
+
+    #######################################################
+    # Helper functions for scripting effects
+    #######################################################
+    def rgb_to_hex(self, red, green, blue):
+        """
+        Converts the specified RGB values into a HEX string.
+
+        Input:  (0, 255, 0)
+        Output: "#00FF00"
+        """
+        return common.rgb_to_hex([red, green, blue])
+
+    def hex_to_rgb(self, value):
+        """
+        Converts a HEX colour string to a RGB list.
+
+        Input:  "#FF0000"
+        Output: [255, 0, 0]
+        """
+        return common.hex_to_rgb(value)
+
+    def saturate_hex(self, hex_value, amount):
+        """
+        Change the saturation of a colour by a relative amount (-1 to 1) and
+        return the new colour hex value.
+
+        Params:
+            hex_value   (str)   Hex value
+            amount      (float) Relative amount to saturate (-1 to 1)
+        """
+        c = colour.Color(hex_value)
+
+        # Value must be between 0 and 1
+        new_value = c.get_saturation() + amount
+        if new_value < 0:
+            new_value = 0
+        elif new_value > 1:
+            new_value = 1
+
+        c.set_saturation(new_value)
+        return c.get_hex_l()
+
+    def saturate_rgb(self, rgb, amount):
+        """
+        Alias to saturate_hex(), but inputs/outputs RGB values.
+
+        Params:
+            rgb         (list)  List of RGB integers [red, green, blue]
+            amount      (float) Relative amount to saturate (-1 to 1)
+        """
+        return self.hex_to_rgb(self.saturate_hex(self.rgb_to_hex(rgb[0], rgb[1], rgb[2]), amount))
+
+    def hue_hex(self, hex_value, amount):
+        """
+        Change the hue of a colour by a relative amount (-1 to 1) and
+        return the new colour hex value.
+
+        Params:
+            hex_value   (str)   Hex value
+            amount      (float) Relative amount to cycle hue (-1 to 1)
+        """
+        c = colour.Color(hex_value)
+        c.set_hue(c.get_hue() + amount)
+        return c.get_hex_l()
+
+    def hue_rgb(self, rgb, amount):
+        """
+        Alias to hue_hex(), but inputs/outputs RGB values.
+
+        Params:
+            rgb         (list)  List of RGB integers [red, green, blue]
+            amount      (float) Relative amount to cycle hue (-1 to 1)
+        """
+        return self.hex_to_rgb(self.hue_hex(self.rgb_to_hex(rgb[0], rgb[1], rgb[2]), amount))
+
+    def lightness_hex(self, hex_value, amount):
+        """
+        Change the lightness of a colour by a relative amount (-1 to 1) and
+        return the new colour hex value.
+
+        Params:
+            hex_value   (str)   Hex value
+            amount      (float) Relative amount to cycle hue (-1 to 1)
+        """
+        c = colour.Color(hex_value)
+
+        # Value must be between 0 and 1
+        new_value = c.get_luminance() + amount
+        if new_value < 0:
+            new_value = 0
+        elif new_value > 1:
+            new_value = 1
+
+        c.set_luminance(new_value)
+        return c.get_hex_l()
+
+    def lightness_rgb(self, rgb, amount):
+        """
+        Alias to lightness_hex(), but inputs/outputs RGB values.
+
+        Params:
+            rgb         (list)  List of RGB integers [red, green, blue]
+            amount      (float) Relative amount to cycle hue (-1 to 1)
+        """
+        return self.hex_to_rgb(self.lightness_hex(self.rgb_to_hex(rgb[0], rgb[1], rgb[2]), amount))
+
+    def gradient(self, colours=[], steps=0):
+        """
+        Return a list of colours that builds a gradient from start to finish
+        using the specified colours (equal distance between each step)
+
+        For best results, make sure the steps are divisable by the number
+        of colours. Otherwise, you may will get more colours then expected.
+
+        For example, ["#000000", "#FFFFFF"] (black, white) across 10 steps
+        makes the 5th colour the midpoint (grey)
+
+        Params:
+            colours     (list)  List of colours for the gradient
+            steps       (int)   Total colours to return for rendering the gradient
+        """
+        if len(colours) < 2:
+            raise ValueError("Insufficient colours! At least 2 required to generate gradient.")
+
+        output = []
+        steps_between_stops = math.ceil(steps / (len(colours) - 1))
+
+        for index, item in enumerate(colours):
+            try:
+                next_colour = colours[index + 1]
+            except IndexError:
+                # This is the last colour
+                continue
+
+            c = colour.Color(item)
+            for c2 in list(c.range_to(next_colour, steps_between_stops)):
+                output.append(c2.get_hex_l())
+
+        return output
---
+++ tests/12_fx.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+#
+# Working directory should be the repository root.
+#
+
+import pylib.fx as fx
+
+import unittest
+
+
+class PolychromaticTests(unittest.TestCase):
+    """
+    Test the internals of Polychromatic.
+    """
+    @classmethod
+    def setUpClass(self):
+        self.fx = fx.FX(6, 22, "Dummy Device", None, "keyboard", "XXXXXXXX")
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_rgb_to_hex(self):
+        self.assertEqual(self.fx.rgb_to_hex(64, 128, 255), "#4080FF", "Cannot convert RGB to HEX")
+
+    def test_hex_to_rgb(self):
+        self.assertEqual(self.fx.hex_to_rgb("#ff8040"), [255, 128, 64], "Cannot convert HEX to RGB")
+
+    def test_saturate_hex(self):
+        # Saturate dull green to a brighter green
+        self.assertEqual(self.fx.saturate_hex("#408040", 1).upper(), "#00C000", "Cannot saturate HEX value")
+
+    def test_saturate_rgb(self):
+        # Same as test above
+        self.assertEqual(self.fx.saturate_rgb([64, 128, 64], 1), [0, 192, 0], "Cannot saturate RGB value")
+
+    def test_hue_hex(self):
+        # Change hue of red to the middle of the HSL colour graph (aqua)
+        self.assertEqual(self.fx.hue_hex("#FF0000", 0.5).upper(), "#00FFFF", "Cannot hue HEX value")
+
+    def test_hue_rgb(self):
+        # Same as test above
+        self.assertEqual(self.fx.hue_rgb([255, 0, 0], 0.5), [0, 255, 255], "Cannot hue RGB value")
+
+    def test_lightness_hex(self):
+        # Reduce lightness of black to grey
+        self.assertEqual(self.fx.lightness_hex("#FFFFFF", -0.5).upper(), "#7F7F7F", "Cannot set lightness of HEX value")
+
+    def test_lightness_rgb(self):
+        # Similar test to above
+        self.assertEqual(self.fx.lightness_rgb([0, 0, 0], 0.5), [127, 127, 127], "Cannot set lightness of RGB value")
+
+    def test_gradient_2_colours(self):
+        # Gradient from black to white, across 10 steps. Midpoint should be grey.
+        gradient = self.fx.gradient(["#000000", "#FFFFFF"], 3)
+        self.assertEqual(gradient[1].upper(), "#7F7F7F", "Cannot verify gradient is accurate")
+
+    def test_gradient_3_colours(self):
+        # Gradient from black to white to black, across 10 steps. Midpoint should be white.
+        gradient = self.fx.gradient(["#000000", "#FFFFFF", "#000000"], 6)
+        self.assertEqual(gradient[2].upper(), "#FFFFFF", "Cannot verify gradient is accurate")
+
+if __name__ == '__main__':
+    unittest.main()
