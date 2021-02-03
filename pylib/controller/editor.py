polychromatic/polychromatic

commit b2fcf4681d1a1b318c77e56cb54560d260cd63b8

editor: Use window placement preferences
The key to remembering dimensions/sizes has been moved from 'controller'
to 'geometry', to organise preferences.json a bit better.

+ Removes deprecated QDesktopWidget() widget

polychromatic-controller
@@ -32,8 +32,7 @@ from PyQt5.QtCore import QThreadPool
 from PyQt5.QtGui import QIcon, QFont, QFontDatabase
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, \
-                            QToolButton, QTabWidget, QAction, QMenu, \
-                            QDesktopWidget
+                            QToolButton, QTabWidget, QAction, QMenu
 
 VERSION = "0.6.0"
 
@@ -109,6 +108,11 @@ class ApplicationData(object):
         self.system_qt_theme = self.preferences["controller"]["system_qt_theme"]
         self.show_menu_bar = self.preferences["controller"]["show_menu_bar"]
 
+        # Preferences Variables
+        self.window_centered = 0
+        self.window_remember = 1
+        self.window_ignore = 2
+
     def load(self):
         """
         Load the application's tab, save data and session data.
@@ -159,10 +163,6 @@ class MainWindow(QMainWindow):
         dbg.stdout("Initalising application...", dbg.action, 1)
         super(MainWindow, self).__init__()
 
-        # Map preferences to variables
-        self.window_behaviour_center = 0
-        self.window_behaviour_remember = 1
-
         # Load UI and locales
         widget = uic.loadUi(os.path.join(common.paths.data_dir, "qt", "main.ui"), self)
         shared.translate_ui(app, widget)
@@ -195,6 +195,51 @@ class MainWindow(QMainWindow):
         self.CloseButton = self.findChild(QPushButton, "CloseApp")
         self.CloseButton.clicked.connect(self.quit_app)
         self.closeEvent = self.quit_app
+
+    @staticmethod
+    def _set_initial_window_position(qmainwindow, key_prefix):
+        """
+        Sets the initial placement of the window, according to user's preferences.
+
+        Params:
+            qmainwindow         QMainWindow() instance
+            key_prefix          Name of prefix for save data. Should be initialized in preferences.
+        """
+        win_behaviour = app.preferences["controller"]["window_behaviour"]
+
+        if win_behaviour == app.window_ignore:
+            return
+
+        if win_behaviour == app.window_centered:
+            frame = qmainwindow.frameGeometry()
+            center = qmainwindow.screen().availableGeometry().center()
+            frame.moveCenter(center)
+            qmainwindow.move(frame.topLeft())
+
+        elif win_behaviour == app.window_remember:
+            dbg.stdout("Loading window geometry...", dbg.action, 1)
+            pos_x = app.preferences["geometry"][key_prefix + "_window_pos_x"]
+            pos_y = app.preferences["geometry"][key_prefix + "_window_pos_y"]
+            size_x = app.preferences["geometry"][key_prefix + "_window_size_x"]
+            size_y = app.preferences["geometry"][key_prefix + "_window_size_y"]
+            qmainwindow.setGeometry(pos_x, pos_y, size_x, size_y)
+
+    @staticmethod
+    def _save_window_position(qmainwindow, key_prefix):
+        """
+        Saves the dimensions and position of the window, according to the user's
+        preferences.
+        """
+        win_behaviour = app.preferences["controller"]["window_behaviour"]
+
+        if win_behaviour == app.window_remember:
+            dbg.stdout("Saving window geometry...", dbg.action, 1)
+            rect = qmainwindow.frameGeometry()
+            app.preferences["geometry"][key_prefix + "_window_pos_x"] = rect.left()
+            app.preferences["geometry"][key_prefix + "_window_pos_y"] = rect.top()
+            app.preferences["geometry"][key_prefix + "_window_size_x"] = rect.width()
+            app.preferences["geometry"][key_prefix + "_window_size_y"] = rect.height()
+            pref.save_file(common.paths.preferences, app.preferences)
 
     def load(self):
         """
@@ -374,24 +419,7 @@ class MainWindow(QMainWindow):
                                     _("Consider updating the application, or reset your save data by deleting: ~/.config/polychromatic")),
                                 traceback=details)
 
-        # Window behaviour
-        #   0 = Open in center
-        #   1 = Remember last position
-        #   2 = Do nothing (let window manager decide)
-
-        win_behaviour = app.preferences["controller"]["window_behaviour"]
-        if win_behaviour == self.window_behaviour_center:
-            rect = self.frameGeometry()
-            center = QDesktopWidget().availableGeometry().center()
-            rect.moveCenter(center)
-            self.move(rect.topLeft())
-
-        elif win_behaviour == self.window_behaviour_remember:
-            pos_x = app.preferences["controller"]["window_pos_x"]
-            pos_y = app.preferences["controller"]["window_pos_y"]
-            size_x = app.preferences["controller"]["window_size_x"]
-            size_y = app.preferences["controller"]["window_size_y"]
-            self.setGeometry(pos_x, pos_y, size_x, size_y)
+        self._set_initial_window_position(self, "main")
 
         # Alternate modes
         if args.open:
@@ -412,14 +440,8 @@ class MainWindow(QMainWindow):
         entirely until the last editor window is closed.
         """
         # Save window position if preference set.
-        if app.preferences["controller"]["window_behaviour"] == app.main_window.window_behaviour_remember:
-            dbg.stdout("Saving window geometry...", dbg.action, 1)
-            rect = app.main_window.frameGeometry()
-            app.preferences["controller"]["window_pos_x"] = rect.left()
-            app.preferences["controller"]["window_pos_y"] = rect.top()
-            app.preferences["controller"]["window_size_x"] = rect.width()
-            app.preferences["controller"]["window_size_y"] = rect.height()
-            pref.save_file(common.paths.preferences, app.preferences)
+        if app.preferences["controller"]["window_behaviour"] == app.window_remember:
+            self._save_window_position(self, "main")
 
         dbg.stdout("Main window closed, goodbye!", dbg.success, 1)
         self.close()

pylib/controller/editor.py
@@ -407,6 +407,26 @@ class VisualEffectEditor(shared.TabData):
             self.dock_layers.deleteLater()
             self.dock_properties.deleteLater()
 
+        # Window geometry
+        self.appdata.main_window._set_initial_window_position(self.window, "editor")
+
+        # -- If opening in the center, make better use of the screen space
+        centered = self.appdata.preferences["controller"]["window_behaviour"] == self.appdata.window_centered
+        already_small = self.window.height() > 1024 and self.window.width() > 768
+        if centered and not already_small:
+            screen = self.window.screen().availableGeometry()
+            frame = self.window.frameGeometry()
+            prefer_height = screen.height() - 175
+            prefer_width = screen.width() - 275
+
+            # Position in the center, as usual
+            center = screen.center()
+            frame.setHeight(prefer_height)
+            frame.setWidth(prefer_width)
+            frame.moveCenter(center)
+            self.window.resize(prefer_width, prefer_height)
+            self.window.move(frame.topLeft())
+
         # Adjust docks for optimum space
         if self.layered_effect:
             self.dock_layers.adjustSize()
@@ -649,6 +669,9 @@ class VisualEffectEditor(shared.TabData):
             self.dbg.stdout("Restoring original device state...", self.dbg.action, 1)
             for zone in self.device["zones"]:
                 self.middleman.replay_active_effect(self.device["backend"], self.device["uid"], zone)
+
+        # Window geometry
+        self.appdata.main_window._save_window_position(self.window, "editor")
 
         # Close window and allow another editor to open again
         self.alive = False

data/qt/editor.ui
@@ -6,9 +6,15 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>1000</width>
+    <width>1050</width>
     <height>700</height>
    </rect>
+  </property>
+  <property name="minimumSize">
+   <size>
+    <width>900</width>
+    <height>600</height>
+   </size>
   </property>
   <property name="windowTitle">
    <string>Effect Editor</string>
@@ -35,7 +41,7 @@
     <rect>
      <x>0</x>
      <y>0</y>
-     <width>1000</width>
+     <width>1050</width>
      <height>30</height>
     </rect>
    </property>

pylib/preferences.py
@@ -68,14 +68,16 @@ def load_file(filepath):
         _validate("controller", "show_menu_bar", bool, True)
         _validate("controller", "system_qt_theme", bool, False)
         _validate("controller", "window_behaviour", int, 0)
-        _validate("controller", "window_pos_x", int, 0)
-        _validate("controller", "window_pos_y", int, 0)
-        _validate("controller", "window_size_x", int, 1000)
-        _validate("controller", "window_size_y", int, 600)
         _validate("tray", "autostart", bool, True)
         _validate("tray", "mode", int, 0)
         _validate("tray", "icon", str, common.get_default_tray_icon())
         _validate("tray", "autostart_delay", int, 0)
+
+        for prefix in ["main", "editor"]:
+            _validate("geometry", prefix + "_window_pos_x", int, 0)
+            _validate("geometry", prefix + "_window_pos_y", int, 0)
+            _validate("geometry", prefix + "_window_size_x", int, 1000)
+            _validate("geometry", prefix + "_window_size_y", int, 600)
 
     return(data)
 

commit ebb6a418021adbabe4831a22864e123b719323dc

Qt: Fix compatibility with older Qt versions
Specifically, Ubuntu 20.04 (Qt 5.12) and 18.04 (Qt 5.8).

Regressed from b2fcf4681d1a1b318c77e56cb54560d260cd63b8

QDesktopWidget() is deprecated, but the new code isn't compatible, so a
conditional has been added to check.

polychromatic-controller
@@ -32,7 +32,8 @@ from PyQt5.QtCore import QThreadPool
 from PyQt5.QtGui import QIcon, QFont, QFontDatabase
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, \
-                            QToolButton, QTabWidget, QAction, QMenu
+                            QToolButton, QTabWidget, QAction, QMenu, \
+                            QDesktopWidget
 
 VERSION = "0.6.0"
 
@@ -212,7 +213,15 @@ class MainWindow(QMainWindow):
 
         if win_behaviour == app.window_centered:
             frame = qmainwindow.frameGeometry()
-            center = qmainwindow.screen().availableGeometry().center()
+
+            # DEPRECATED: QDesktopWidget(), but some distros ship an older Qt version (5.8, 5.12)
+            qt_ver = QtCore.PYQT_VERSION_STR.split(".")
+            if qt_ver[0] == "5" and int(qt_ver[1]) <= 14:
+                center = QDesktopWidget().availableGeometry().center()
+            else:
+                # For >= Qt 5.15
+                center = qmainwindow.screen().availableGeometry().center()
+
             frame.moveCenter(center)
             qmainwindow.move(frame.topLeft())
 

pylib/controller/editor.py
@@ -23,6 +23,7 @@ import json
 import os
 import time
 
+from PyQt5 import QtCore
 from PyQt5.QtCore import Qt, QRect, QItemSelectionModel, QThread, QSize, QUrl
 from PyQt5.QtGui import QIcon, QPixmap, QFont
 from PyQt5.QtWidgets import QWidget, QPushButton, QToolButton, QMessageBox, \
@@ -32,7 +33,7 @@ from PyQt5.QtWidgets import QWidget, QPushButton, QToolButton, QMessageBox, \
                             QGroupBox, QRadioButton, QMainWindow, QAction, \
                             QDockWidget, QMenuBar, QToolBar, QStatusBar, \
                             QTableWidget, QWidget, QVBoxLayout, QHBoxLayout, \
-                            QScrollArea, QSpinBox
+                            QScrollArea, QSpinBox, QDesktopWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 # Visual mode within the WebView editor
@@ -416,7 +417,15 @@ class VisualEffectEditor(shared.TabData):
         centered = self.appdata.preferences["controller"]["window_behaviour"] == self.appdata.window_centered
         already_small = self.window.height() > 1024 and self.window.width() > 768
         if centered and not already_small:
-            screen = self.window.screen().availableGeometry()
+
+            # DEPRECATED: QDesktopWidget(), but some distros ship an older Qt version (5.8, 5.12)
+            qt_ver = QtCore.PYQT_VERSION_STR.split(".")
+            if qt_ver[0] == "5" and int(qt_ver[1]) <= 14:
+                screen = QDesktopWidget().availableGeometry()
+            else:
+                # For >= Qt 5.15
+                screen = self.window.screen().availableGeometry()
+
             frame = self.window.frameGeometry()
             prefer_height = screen.height() - 175
             prefer_width = screen.width() - 275