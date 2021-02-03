97836d778aedf88c1e96f15f686c682237d66f25
Luke Horwell <code@horwell.me>
2021-02-03 15:42:27 +0000

    CI: Produce packages for polychromatic/edge PPA
    For Ubuntu users, ppa:polychromatic/daily has been deprecated and
    replaced with ppa:polychromatic/edge.
    
    When there are new commits, they'll be dispatched directly to Launchpad.
    Intended for testers looking for the absolutely latest code to test. As
    usual, this comes with a "unstable" label.

---
+++ scripts/packaging/generate-edge-debian-changelog.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+#
+# This script is used by the CI system to produce changelogs for automated
+# Debian/Ubuntu builds.
+#
+# Parameters: <codename>
+#
+
+import datetime
+import os
+import subprocess
+import sys
+
+CODENAME = sys.argv[1]
+CHANGELOG = os.path.join(os.path.dirname(__file__), "../../debian/changelog")
+PKG_VERSION = "0.0.0"
+
+
+def run_command(cmd):
+    cmd = cmd.split(" ")
+    return str(subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].decode("UTF-8")).strip()
+
+
+# Determine package version
+ver_raw = run_command("git describe")[1:]
+ver_parts = ver_raw.split("-")
+if len(ver_parts) == 3:
+    # Example: 0.6.0-41-g59319b6
+    PKG_VERSION = "{0}-{1}~{2}".format(ver_parts[0], ver_parts[1], ver_parts[2])
+    START_TAG = "v" + ver_parts[0]
+else:
+    # Example: 1.0.0
+    PKG_VERSION = ver_raw
+    START_TAG = "v" + ver_raw
+
+
+# Generate a changelog to describe latest changes
+LOG = run_command("git log {0}..HEAD --oneline".format(START_TAG)).strip()
+SIGN_DATE = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S")
+
+def get_changelog():
+    lines = []
+    lines.append("polychromatic ({0}) {1}; urgency=low\n\n".format(PKG_VERSION, CODENAME))
+    lines.append("  This package was automatically generated.\n\n")
+    if len(LOG.split("\n")) > 1:
+        lines.append("  Commit changes (newest first):\n\n")
+        for line in LOG.split("\n"):
+            lines.append("  * " + line + "\n")
+    lines.append("\n -- Polychromatic Builder <bot@polychromatic.app>  {0} +0000\n\n".format(SIGN_DATE))
+
+    return "".join(lines)
+
+
+# Generate and upload each package
+with open(CHANGELOG, "w") as f:
+    f.writelines(get_changelog())
---
+++ scripts/packaging/prepare-edge-ppa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This script prepares the Edge PPA containing the latest builds for Ubuntu.
+#
+# Expected to be run by CI system.
+# Parameters: <path to src code>
+#
+
+REPO_ROOT="$(realpath "$1")"
+
+for codename in "bionic" "focal" "groovy" "hirsute"
+do
+    echo -e "\n$codename"
+    echo -e "===================="
+    TEMP_DIR="$(mktemp -d)"
+    git clone "$REPO_ROOT" "$TEMP_DIR/src"
+    cd "$TEMP_DIR/src"
+    ./scripts/packaging/generate-edge-debian-changelog.py $codename
+    debuild -S
+    debsign -k 49D6E0C94C9832E63FDBD50BEAF6D6A2C65D1D85 ../*.changes
+    dput ppa:polychromatic/edge ../*.changes
+done
