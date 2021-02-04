45f7ff52c55a4390e0ebe226eb0c3e6472118072
Luke Horwell <code@horwell.me>
2021-02-04 15:08:36 +0000

    CI: Skip edge PPA builds for Ubuntu 18.04
    Ubuntu 18.04 "bionic" is throwing an error on Launchpad, and a different
    one in a local VM. Possibly because of the version of meson.
    
    While 18.04 is intended to be a supported release when the app goes
    stable, time would be better spent elsewhere then digging into this
    packaging issue at this time.

--- scripts/packaging/prepare-edge-ppa.sh
+++ scripts/packaging/prepare-edge-ppa.sh
@@ -8,7 +8,7 @@
 
 REPO_ROOT="$(realpath "$1")"
 
-for codename in "bionic" "focal" "groovy" "hirsute"
+for codename in "focal" "groovy" "hirsute"
 do
     echo -e "\n$codename"
     echo -e "===================="
