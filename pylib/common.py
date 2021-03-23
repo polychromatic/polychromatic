polychromatic/polychromatic

commit f995a8ef7abf18a915f574b1ae004b6d0d5f38cf

common: Restore current directory after getting git version

pylib/common.py
@@ -544,23 +544,28 @@ def get_versions(base_version):
 
     if os.path.exists(os.path.join(os.path.dirname(__file__), "..", ".git")):
         import subprocess
+
+        git_commit = None
+        old_path = os.getcwd()
         os.chdir(os.path.dirname(__file__))
 
         try:
             git_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("UTF-8")
 
             try:
-                git_version = subprocess.check_output(["git", "describe"]).strip().decode("UTF-8")[1:]
+                version = subprocess.check_output(["git", "describe"]).strip().decode("UTF-8")[1:]
             except subprocess.CalledProcessError:
                 # Git may throw error "no names found" if repository was shallow cloned or has no tags
-                git_version = "{0}-git-{1}".format(base_version, git_commit[:7])
-            return (git_version, git_commit, py_version)
+                version = "{0}-git-{1}".format(base_version, git_commit[:7])
+
         except FileNotFoundError:
             # "git" is not installed despite being a git repository
-            base_version = base_version + "-git"
+            version = base_version + "-git"
+
+        os.chdir(old_path)
 
     # Production "installed" version
-    return (base_version, None, py_version)
+    return (version, git_commit, py_version)
 
 
 # Available to all modules

commit 7432284b2e865ff27cc93dbedf02c21f2bf76385

common: Fix regression getting version info
Regressed from f995a8ef7abf18a915f574b1ae004b6d0d5f38cf
Fixes #317.

pylib/common.py
@@ -540,6 +540,7 @@ def get_versions(base_version):
 
     This is intended to make debugging easier.
     """
+    version = base_version
     py_version = "{0}.{1}.{2}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
 
     if os.path.exists(os.path.join(os.path.dirname(__file__), "..", ".git")):