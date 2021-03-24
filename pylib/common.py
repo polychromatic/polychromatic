polychromatic/polychromatic

commit 8fd6edfba535b818f77ecf847350d1da4b2d1f87

Prevent crash running from repository when 'git' not installed

pylib/common.py
@@ -546,14 +546,18 @@ def get_versions(base_version):
         import subprocess
         os.chdir(os.path.dirname(__file__))
 
-        git_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("UTF-8")
-
         try:
-            git_version = subprocess.check_output(["git", "describe"]).strip().decode("UTF-8")[1:]
-        except subprocess.CalledProcessError:
-            # Git may throw error "no names found" if repository was shallow cloned or has no tags
-            git_version = "{0}-git-{1}".format(base_version, git_commit[:7])
-        return (git_version, git_commit, py_version)
+            git_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("UTF-8")
+
+            try:
+                git_version = subprocess.check_output(["git", "describe"]).strip().decode("UTF-8")[1:]
+            except subprocess.CalledProcessError:
+                # Git may throw error "no names found" if repository was shallow cloned or has no tags
+                git_version = "{0}-git-{1}".format(base_version, git_commit[:7])
+            return (git_version, git_commit, py_version)
+        except FileNotFoundError:
+            # "git" is not installed despite being a git repository
+            base_version = base_version + "-git"
 
     # Production "installed" version
     return (base_version, None, py_version)

commit 390ed7e6c94d1c3c04f374a5755af8874327310c

Fix regression for checking version
Fixes #318.

Regression from 8fd6edfba535b818f77ecf847350d1da4b2d1f87 and
7432284b2e865ff27cc93dbedf02c21f2bf76385 did not fully fix it.

pylib/common.py
@@ -541,12 +541,12 @@ def get_versions(base_version):
     This is intended to make debugging easier.
     """
     version = base_version
+    git_commit = None
     py_version = "{0}.{1}.{2}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
 
     if os.path.exists(os.path.join(os.path.dirname(__file__), "..", ".git")):
         import subprocess
 
-        git_commit = None
         old_path = os.getcwd()
         os.chdir(os.path.dirname(__file__))
 