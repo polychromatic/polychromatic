polychromatic/polychromatic

commit 44d4a0567120db9055b0101596baca92589e138b

controller: Change initialisation order
The Controller window appears sooner, with a "skeleton" loading screen
for the devices tab, just in case backends (i.e. OpenRazer) take longer
then expected to initalize.

pylib/controller/effects.py
@@ -33,9 +33,6 @@ class EffectsTab(shared.CommonFileTab):
     def __init__(self, appdata):
         super().__init__(appdata, effects.EffectFileManagement, "EffectContents", "EffectSidebarTree")
 
-        # Set up variables
-        self.feature = "effects"
-
         # Populate tasks
         self.tasks = {
             "new": self.new_file,
@@ -186,6 +183,35 @@ class EffectsTab(shared.CommonFileTab):
             # Refresh the UI for when the user returns
             self.set_tab()
             self.open_file(path)
+
+        # Backends not ready - cannot show device list
+        if not self.appdata.ready:
+            self.dbg.stdout("Backends not ready! Device list will not be available for new effect.", self.dbg.warning, 1)
+            action = ""
+
+            def do_ignore():
+                # Let the metadata editor handle no devices
+                pass
+
+            def do_retry():
+                nonlocal action
+                action = "retry"
+
+            def do_cancel():
+                nonlocal action
+                action = "cancel"
+
+            self.widgets.open_dialog(self.widgets.dialog_warning,
+                                        self._("Backend Not Ready"),
+                                        self._("The application is still waiting for the backends to finish initialising so you can pick a device to map your new effect."),
+                                        self._("This might be ready in a few moments."),
+                                        buttons=[QMessageBox.Cancel, QMessageBox.Ignore, QMessageBox.Retry],
+                                        actions={QMessageBox.Ignore: do_ignore, QMessageBox.Retry: do_retry})
+
+            if action == "cancel":
+                return
+            elif action == "retry":
+                return self.new_file_stage_2(old_dialog, effect_type)
 
         metadata_editor = EffectMetadataEditor(self.appdata, data, _save_metadata)
 

data/img/general/placeholder.svg
@@ -1,0 +1,1 @@
+<svg width="24" height="24" version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect width="24" height="24" rx="12" ry="12" style="fill:#202020"/></svg>

data/qt/main.ui
@@ -926,6 +926,9 @@
    </property>
   </action>
   <action name="actionOpenRazerRestartDaemon">
+   <property name="enabled">
+    <bool>false</bool>
+   </property>
    <property name="icon">
     <iconset theme="view-refresh"/>
    </property>
@@ -934,6 +937,9 @@
    </property>
   </action>
   <action name="actionOpenRazerOpenLog">
+   <property name="enabled">
+    <bool>false</bool>
+   </property>
    <property name="icon">
     <iconset theme="folder"/>
    </property>
@@ -942,6 +948,9 @@
    </property>
   </action>
   <action name="actionOpenRazerConfigure">
+   <property name="enabled">
+    <bool>false</bool>
+   </property>
    <property name="icon">
     <iconset theme="settings-configure"/>
    </property>
@@ -950,6 +959,9 @@
    </property>
   </action>
   <action name="actionOpenRazerAbout">
+   <property name="enabled">
+    <bool>false</bool>
+   </property>
    <property name="icon">
     <iconset>
      <normaloff>../img/logo/openrazer.svg</normaloff>../img/logo/openrazer.svg</iconset>
@@ -1039,6 +1051,9 @@
    </property>
   </action>
   <action name="actionTroubleshootOpenRazer">
+   <property name="enabled">
+    <bool>false</bool>
+   </property>
    <property name="icon">
     <iconset>
      <normaloff>../img/logo/openrazer.svg</normaloff>../img/logo/openrazer.svg</iconset>

polychromatic-controller
@@ -25,10 +25,11 @@ import os
 import signal
 import threading
 import setproctitle
+import time
 import sys
 
 from PyQt5 import uic, QtCore
-from PyQt5.QtCore import QThreadPool
+from PyQt5.QtCore import QThread
 from PyQt5.QtGui import QIcon, QFont, QFontDatabase
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, \
@@ -76,6 +77,7 @@ class ApplicationData(object):
         """
         Prepare the object.
         """
+        dbg.stdout("Initalising application...", dbg.action, 1)
         self.main_window = None
         self.main_app = qapp
         self.dbg = dbg
@@ -98,13 +100,17 @@ class ApplicationData(object):
 
         # Assigned in load() later
         self.middleman = None
-        self.menubar = None
+        self.ready = False
         self.device_list = []
-        self.current_device = None
-        self.effect_list = None
+
+        self.menubar = None
+        self.tab_devices = None
+        self.tab_effects = None
+        self.tab_presets = None
+        self.tab_triggers = None
+        self.ui_preferences = None
 
         # Settings
-        dbg.stdout("Loading preferences...", dbg.action, 1)
         self.preferences = pref.load_file(self.paths.preferences)
         self.system_qt_theme = self.preferences["controller"]["system_qt_theme"]
         self.show_menu_bar = self.preferences["controller"]["show_menu_bar"]
@@ -116,44 +122,61 @@ class ApplicationData(object):
 
     def load(self):
         """
-        Load the application's tab, save data and session data.
+        Show the main window and spawn a thread to load the rest of the application.
         """
         self.main_window = MainWindow()
-
-        # Polychromatic's Qt theme is based on Fusion, for consistency.
-        if not self.system_qt_theme:
-            qapp.setStyle("Fusion")
-
-        # Misc
-        self.menubar = controller.menubar.MenuBar(self)
-
-        # Session Data
-        dbg.stdout("Getting device list...", dbg.action, 1)
         self.middleman = middleman.Middleman(dbg, common, _)
-        self.middleman.init()
-        self.device_list = self.middleman.get_device_list()
-        self.current_device = None
 
-        # Tabs
+        # Menu bar & tabs
         self.tab_devices = controller.devices.DevicesTab(self)
         self.tab_effects = controller.effects.EffectsTab(self)
         self.tab_presets = controller.presets.PresetsTab(self)
         self.tab_triggers = controller.triggers.TriggersTab(self)
+        self.menubar = controller.menubar.MenuBar(self)
 
         # Subwindows
         self.ui_preferences = controller.preferences.PreferencesWindow(self)
 
-        # Enable active backends in menu bar
-        # -- OpenRazer
-        if "openrazer" in self.middleman.not_installed:
-            for action in ["actionOpenRazerConfigure",
-                           "actionOpenRazerReportBug",
-                           "actionOpenRazerOpenLog",
-                           "actionOpenRazerRestartDaemon",
-                           "actionOpenRazerAbout"]:
-                self.main_window.findChild(QAction, action).setEnabled(False)
-
+        self.init_bg_thread()
         self.main_window.load()
+
+    def init_bg_thread(self):
+        """
+        Backends will be initialised in this thread.
+        """
+        class BackgroundThread(QThread):
+            @staticmethod
+            def run():
+                t_start = time.time()
+                self.dbg.stdout("Initalising backends...", dbg.action, 1)
+                self.middleman.init()
+                self.device_list = self.middleman.get_device_list()
+
+                def _enable_action(action, enabled=True):
+                    self.main_window.findChild(QAction, action).setEnabled(enabled)
+
+                # Enable OpenRazer functions if available
+                # See also: pylib.controller.preferences.refresh_backend_status()
+                if not "openrazer" in self.middleman.not_installed:
+                    _enable_action("actionOpenRazerConfigure")
+                    _enable_action("actionOpenRazerOpenLog")
+                    _enable_action("actionOpenRazerRestartDaemon")
+                    _enable_action("actionOpenRazerAbout")
+
+                if "openrazer" in self.middleman.import_errors.keys():
+                    _enable_action("actionOpenRazerAbout", False)
+
+                if "openrazer" in self.middleman.troubleshooters:
+                    _enable_action("actionTroubleshootOpenRazer")
+
+                t_end = time.time()
+                self.dbg.stdout("Backends loaded in {0}s.".format(str(round(t_end - t_start, 3))), dbg.action, 1)
+                self.ready = True
+
+                app.ui_preferences.refresh_backend_status()
+
+        self.bg_thread = BackgroundThread()
+        self.bg_thread.start()
 
 
 class MainWindow(QMainWindow):
@@ -161,7 +184,7 @@ class MainWindow(QMainWindow):
     The main window is the first window the user will use and interact with.
     """
     def __init__(self):
-        dbg.stdout("Initalising application...", dbg.action, 1)
+        dbg.stdout("Initalising window...", dbg.action, 1)
         super(MainWindow, self).__init__()
 
         # BUG: The "gtk2" theme causes a hang under OpenRazer's Python library (#317)
@@ -185,17 +208,14 @@ you have selected 'Use system Qt theme', which may trigger the bug.
         widget = uic.loadUi(os.path.join(common.paths.data_dir, "qt", "main.ui"), self)
         shared.translate_ui(app, widget)
 
-        header = self.findChild(QWidget, "Header")
-        tabs = self.findChild(QWidget, "MainTabWidget")
-        tabs_custom = self.findChild(QWidget, "MainTabCustom")
-
+        # Show the correct tab widgets
         if app.system_qt_theme:
             # User prefers own Qt theme, use native tabs.
-            header.hide()
-            tabs_custom.hide()
+            self.findChild(QWidget, "Header").hide()
+            self.findChild(QWidget, "MainTabCustom").hide()
         else:
             # Custom Qt theme uses a different tab design. Hide native tabs.
-            tabs.tabBar().hide()
+            self.findChild(QWidget, "MainTabWidget").tabBar().hide()
 
         # Set window attributes
         self.setWindowIcon(QIcon(common.get_icon("general", "controller.svg")))
@@ -272,20 +292,19 @@ you have selected 'Use system Qt theme', which may trigger the bug.
 
     def load(self):
         """
-        Objects initalised. Continue loading the main application window
-        containing all the tabs.
+        Objects initalised. Proceed to load the main application window.
         """
-        # Finish loading UI style/fonts
-        QFontDatabase.addApplicationFont(os.path.join(common.paths.data_dir, "qt", "fonts", "Play_regular.ttf"))
-        controller.shared.load_qt_theme(app, self)
+        widgets = shared.PolychromaticWidgets(app)
 
+        # Polychromatic's Qt theme is based on Fusion, uses the "Play" font.
         if not app.system_qt_theme:
+            qapp.setStyle("Fusion")
+            QFontDatabase.addApplicationFont(os.path.join(common.paths.data_dir, "qt", "fonts", "Play_regular.ttf"))
             qapp.setFont(QFont("Play", 10, 0))
+            controller.shared.load_qt_theme(app, self)
 
-        # Tab objects - custom design and native tabs
+        # Prepare "native" tab widget and custom buttons acting as tabs for the design.
         tabs = self.findChild(QTabWidget, "MainTabWidget")
-
-        # Prepare events when clicking on custom tabs or real tabs:
         tab_buttons = [
             self.findChild(QToolButton, "DevicesTabButton"),
             self.findChild(QToolButton, "EffectsTabButton"),
@@ -293,7 +312,7 @@ you have selected 'Use system Qt theme', which may trigger the bug.
             self.findChild(QToolButton, "TriggersTabButton")
         ]
 
-        # Also in the menu bar
+        # Also in the menu bar's view menu
         tab_menu_items = [
             self.findChild(QAction, "actionDevices"),
             self.findChild(QAction, "actionEffects"),
@@ -302,50 +321,12 @@ you have selected 'Use system Qt theme', which may trigger the bug.
         ]
 
         # Tab icons
-        widgets = shared.PolychromaticWidgets(app)
         tab_buttons[0].setIcon(widgets.get_icon_qt("general", "devices"))
         tab_buttons[1].setIcon(widgets.get_icon_qt("general", "effects"))
         tab_buttons[2].setIcon(widgets.get_icon_qt("general", "presets"))
         tab_buttons[3].setIcon(widgets.get_icon_qt("general", "triggers"))
 
-        # Load button icons
-        if not app.system_qt_theme:
-            # -- File
-            self.findChild(QAction, "actionNewEffect").setIcon(widgets.get_icon_qt("general", "effects"))
-            self.findChild(QAction, "actionImportEffect").setIcon(widgets.get_icon_qt("general", "import"))
-            self.findChild(QAction, "actionNewPreset").setIcon(widgets.get_icon_qt("general", "presets"))
-            self.findChild(QAction, "actionNewPresetNow").setIcon(widgets.get_icon_qt("devices", "all"))
-            self.findChild(QAction, "actionQuitApp").setIcon(widgets.get_icon_qt("general", "exit"))
-
-            # -- Edit
-            self.findChild(QAction, "actionDuplicate").setIcon(widgets.get_icon_qt("general", "clone"))
-            self.findChild(QAction, "actionDelete").setIcon(widgets.get_icon_qt("general", "delete"))
-            self.findChild(QAction, "actionRefreshTab").setIcon(widgets.get_icon_qt("general", "refresh"))
-            self.findChild(QAction, "actionPreferences").setIcon(widgets.get_icon_qt("general", "preferences"))
-
-            # -- View
-            self.findChild(QAction, "actionDevices").setIcon(widgets.get_icon_qt("general", "devices"))
-            self.findChild(QAction, "actionEffects").setIcon(widgets.get_icon_qt("general", "effects"))
-            self.findChild(QAction, "actionPresets").setIcon(widgets.get_icon_qt("general", "presets"))
-            self.findChild(QAction, "actionTriggers").setIcon(widgets.get_icon_qt("general", "triggers"))
-            self.findChild(QAction, "actionReinstateMenuBar").setIcon(widgets.get_icon_qt("general", "pin"))
-            self.findChild(QAction, "actionHideMenuBar").setIcon(widgets.get_icon_qt("general", "pin"))
-
-            # -- Tools
-            self.findChild(QMenu, "menuTroubleshoot").setIcon(widgets.get_icon_qt("emblems", "utility"))
-            self.findChild(QAction, "actionRestartTrayApplet").setIcon(widgets.get_icon_qt("general", "tray-applet"))
-            self.findChild(QAction, "actionProcessViewer").setIcon(widgets.get_icon_qt("emblems", "software"))
-
-            # -- Tools/OpenRazer
-            self.findChild(QAction, "actionOpenRazerWebsite").setIcon(widgets.get_icon_qt("general", "external"))
-            self.findChild(QAction, "actionOpenRazerConfigure").setIcon(widgets.get_icon_qt("general", "preferences"))
-            self.findChild(QAction, "actionOpenRazerOpenLog").setIcon(widgets.get_icon_qt("general", "folder"))
-            self.findChild(QAction, "actionOpenRazerRestartDaemon").setIcon(widgets.get_icon_qt("general", "refresh"))
-
-            # -- Help
-            self.findChild(QAction, "actionOnlineHelp").setIcon(widgets.get_icon_qt("general", "external"))
-
-        # Code for each tab is stored in a separate module
+        # Each tab stores its logic in a separate module
         tab_objects = [
             app.tab_devices,
             app.tab_effects,
@@ -370,7 +351,6 @@ you have selected 'Use system Qt theme', which may trigger the bug.
             except Exception as e:
                 traceback = common.get_exception_as_string(e)
                 print(traceback)
-                widgets = controller.shared.PolychromaticWidgets(app)
                 widgets.open_dialog(widgets.dialog_error,
                                     _("Controller Error"),
                                     _("Eeks! An uncaught exception was thrown while loading the tab.") + '\n\n' + \
@@ -391,7 +371,7 @@ you have selected 'Use system Qt theme', which may trigger the bug.
         # Press F5 to refresh tab
         self.findChild(QAction, "actionRefreshTab").triggered.connect(_change_tab)
 
-        # TODO: Move to a loop, but watch out for variable references
+        # Connect custom tab buttons/view menu for tab switching
         tab_buttons[0].clicked.connect(lambda a: _change_tab_proxy(a, 0))
         tab_buttons[1].clicked.connect(lambda a: _change_tab_proxy(a, 1))
         tab_buttons[2].clicked.connect(lambda a: _change_tab_proxy(a, 2))
@@ -415,7 +395,7 @@ you have selected 'Use system Qt theme', which may trigger the bug.
             # Signal triggered upon switching tab in the widget.
             _change_tab_proxy(None, landing_tab)
 
-            # Signal is not triggered as the starting page is already 0.
+            # Signal may not trigger if the starting page is already 0.
             if landing_tab == 0:
                 _change_tab()
 
@@ -426,7 +406,6 @@ you have selected 'Use system Qt theme', which may trigger the bug.
         pref_ver = pref.VERSION
         save_ver = app.preferences["config_version"]
         if save_ver > pref_ver:
-            widgets = controller.shared.PolychromaticWidgets(app)
             details = "Polychromatic v{0}\nSave Data: {1} (application requires {2} or older)".format(VERSION, save_ver, pref_ver)
             widgets.open_dialog(widgets.dialog_warning,
                                 _("Incompatible Save Data"),
@@ -438,7 +417,7 @@ you have selected 'Use system Qt theme', which may trigger the bug.
 
         self._set_initial_window_position(self, "main")
 
-        # Alternate modes
+        # Minimal modes
         if args.open:
             if args.open == "troubleshoot":
                 # TODO: Should support multiple backends!
@@ -496,7 +475,7 @@ def parse_parameters():
     """
     global _
 
-    tab_choices = [
+    open_choices = [
         "devices",
         "effects",
         "presets",
@@ -511,7 +490,7 @@ def parse_parameters():
     parser.add_argument("--version", help=_("Print program version and exit"), action="store_true")
     parser.add_argument("-v", "--verbose", help=_("Be verbose to stdout"), action="store_true")
     parser.add_argument("--locale", help=_("Force a specific language, e.g. de_DE"), action="store")
-    parser.add_argument("--open", help=_("Open a specific tab or feature"), action="store", choices=tab_choices)
+    parser.add_argument("--open", help=_("Open a specific tab or feature"), action="store", choices=open_choices)
 
     args = parser.parse_args()
 

pylib/controller/devices.py
@@ -14,14 +14,16 @@ from .. import preferences as pref
 from .. import middleman
 from . import shared
 
-from PyQt5.QtCore import Qt, QSize, QMargins
+import time
+
+from PyQt5.QtCore import Qt, QSize, QMargins, QThread
 from PyQt5.QtGui import QIcon, QPixmap, QFont
 from PyQt5.QtWidgets import QWidget, QScrollArea, QGroupBox, QGridLayout, \
                             QPushButton, QToolButton, QMessageBox, QListWidget, \
                             QTreeWidget, QTreeWidgetItem, QLabel, QComboBox, \
                             QSpacerItem, QSizePolicy, QSlider, QCheckBox, \
                             QButtonGroup, QRadioButton, QDialog, QTableWidget, \
-                            QTableWidgetItem
+                            QTableWidgetItem, QAction
 
 # Error codes
 ERROR_NO_DEVICE = 0
@@ -40,13 +42,14 @@ class DevicesTab(shared.TabData):
         self.current_backend = None
         self.current_uid = None
         self.current_serial = None
+        self.load_thread = None
 
         # UI Elements
         self.Contents = self.main_window.findChild(QWidget, "DeviceContents")
         self.SidebarTree = self.main_window.findChild(QTreeWidget, "DeviceSidebarTree")
         self.SidebarTree.itemClicked.connect(self._sidebar_changed)
 
-        # Avoid GC from cleaning up invisible controls
+        # Avoid garbage collection cleaning up invisible controls
         self.btn_grps = {}
 
     def set_tab(self):
@@ -56,6 +59,7 @@ class DevicesTab(shared.TabData):
         """
         # Open all 'sidebar' tree branches
         self.SidebarTree.expandAll()
+        self.SidebarTree.setEnabled(True)
 
         # Populate sidebar
         tasks_branch = self.SidebarTree.invisibleRootItem().child(0)
@@ -65,6 +69,11 @@ class DevicesTab(shared.TabData):
         tasks_branch.section = None
         devices_branch.section = None
         tasks_branch.child(0).section = "apply-to-all"
+
+        # Backends are still initialising
+        if not self.appdata.ready:
+            self._open_loading()
+            return
 
         # Recache the device list
         self.SidebarTree.parent().show()
@@ -590,6 +599,110 @@ class DevicesTab(shared.TabData):
                                      _("The request could not be completed due to an error."),
                                      traceback=response)
 
+    def _open_loading(self):
+        """
+        Show a skeleton loading screen while backends finish loading.
+
+        A thread will spawn to refresh the devices tab (if open) when the
+        middleman has fully populated.
+        """
+        if self.load_thread:
+            return
+
+        # Sidebar
+        self.SidebarTree.setEnabled(False)
+        tasks_branch = self.SidebarTree.invisibleRootItem().child(0)
+        devices_branch = self.SidebarTree.invisibleRootItem().child(1)
+        tasks_branch.setHidden(True)
+
+        # Skeleton devices
+        device_icon = QIcon()
+        device_icon.addFile(common.get_icon("general", "placeholder"), mode=QIcon.Disabled)
+        skel_strings = [
+            "█████████████",
+            "████████████",
+            "█████████",
+            "███████████",
+            "███████",
+        ]
+        for i in range(0, 5):
+            skel = QTreeWidgetItem()
+            skel.setText(0, skel_strings[i])
+            skel.setIcon(0, device_icon)
+            skel.setDisabled(True)
+            devices_branch.addChild(skel)
+
+        # Skeleton summary
+        def dummy():
+            pass
+
+        buttons = []
+        indicators = [
+            {
+                "icon": common.get_icon("general", "placeholder"),
+                "label": "██████"
+            },
+            {
+                "icon": common.get_icon("general", "placeholder"),
+                "label": "███"
+            }
+        ]
+        summary = self.widgets.create_summary_widget(common.get_icon("devices", "noimage"), "████████████", indicators, buttons)
+
+        # Skeleton device controls (brightness & effect)
+        skels_text = summary.findChildren(QLabel)
+        skels_bg = []
+
+        dummy_slider = QLabel("________________________")
+        dummy_slider.setMaximumWidth(180)
+        dummy_slider.setAlignment(Qt.AlignLeft)
+        skels_bg.append(dummy_slider)
+
+        dummy_buttons = []
+        for i in range(0, 5):
+            dummy = QLabel()
+            dummy.setMinimumHeight(70)
+            dummy.setMinimumWidth(70)
+            dummy.setMaximumHeight(70)
+            dummy.setMaximumWidth(70)
+            dummy.setAlignment(Qt.AlignLeft)
+            dummy_buttons.append(dummy)
+            skels_bg.append(dummy)
+
+        row1 = self.widgets.create_row_widget("███████", [dummy_slider], wrap=True)
+        row2 = self.widgets.create_row_widget("████", dummy_buttons, wrap=True)
+
+        label1 = row1.findChild(QLabel)
+        label2 = row2.findChild(QLabel)
+
+        skels_text.append(label1)
+        skels_text.append(label2)
+
+        # Apply skeleton styling
+        for skel in skels_bg:
+            skel.setStyleSheet("QLabel { background-color: #202020; color: #202020; margin: 4px 0; }")
+        for skel in skels_text:
+            skel.setStyleSheet("QLabel { color: #202020; }")
+
+        # Append widgets
+        layout = self.Contents.layout()
+        for widget in [summary, row1, row2]:
+            layout.addWidget(widget)
+        layout.addStretch()
+
+        class WaitForBackendThread(QThread):
+            @staticmethod
+            def run():
+                while not self.appdata.ready:
+                    time.sleep(0.05)
+
+                refresh = self.main_window.findChild(QAction, "actionRefreshTab")
+                refresh.trigger()
+
+        self.load_thread = WaitForBackendThread()
+        self.load_thread.start()
+        self.set_cursor_busy()
+
     def _open_no_backend_found(self, message_id):
         """
         No backends are present. Hide the sidebar and show a full screen message.
@@ -659,7 +772,7 @@ class DevicesTab(shared.TabData):
         Opens the documentation online to learn more about Polychromatic's device
         functionality.
         """
-        self.menubar.online_help()
+        self.appdata.menubar.online_help()
 
     def _start_troubleshooter(self):
         """
@@ -668,7 +781,7 @@ class DevicesTab(shared.TabData):
         to troubleshoot.
         """
         # TODO: Prompt not implemented as only one backend (OpenRazer)
-        self.menubar.openrazer.troubleshoot()
+        self.appdata.menubar.openrazer.troubleshoot()
 
     def _open_backend_exception(self):
         """
@@ -680,12 +793,12 @@ class DevicesTab(shared.TabData):
         for backend_id in self.middleman.import_errors.keys():
             backend_name = middleman.BACKEND_ID_NAMES[backend_id]
             exception = self.middleman.import_errors[backend_id].strip()
-            dbg.stdout("{0}\n------------------------------\n{1}".format(backend_name, exception), dbg.error)
+            dbg.stdout("\n{0}\n------------------------------\n{1}\n".format(backend_name, exception), dbg.error)
             self.widgets.open_dialog(self.widgets.dialog_generic,
                                     _("Backend Error: []").replace("[]", backend_name),
-                                    _("The last line of the exception was:") + "\n\n" + exception.split("\n")[-1],
-                                    None,
-                                    exception)
+                                    _("Polychromatic won't be able to interact with devices available under [] until the problem has been resolved.").replace("[]", backend_name),
+                                    info_text=_("The last line of the exception was:") + "\n" + exception.split("\n")[-1],
+                                    traceback=exception)
 
     def open_unknown_device(self, backend):
         """

pylib/controller/editor.py
@@ -533,6 +533,28 @@ class VisualEffectEditor(shared.TabData):
         self.dbg.stdout("Preparing live preview on '{0}'...".format(device_name), self.dbg.action, 1)
         self.device = self.middleman.get_device_by_name(device_name)
 
+        # Inform the user if the backends are not ready
+        if not self.appdata.ready:
+            self.dbg.stdout("Backends not ready! Preview not possible yet.", self.dbg.error, 1)
+            retry = False
+
+            def do_retry():
+                nonlocal retry
+                retry = True
+
+            def do_ignore():
+                self.dbg.stdout("Loading editor without live preview.", self.dbg.action, 1)
+
+            self.widgets.open_dialog(self.widgets.dialog_warning,
+                                        self._("Backend Error"),
+                                        self._("Live preview isn't possible yet as the backends haven't finished initialising."),
+                                        self._("They might be ready now, try again. Or, choose Ignore to continue without a live preview on physical hardware."),
+                                        buttons=[QMessageBox.Retry, QMessageBox.Ignore],
+                                        actions={QMessageBox.Retry: do_retry, QMessageBox.Ignore: do_ignore})
+
+            if retry:
+                return self.init_device_preview()
+
         # Inform the user if attempting to load non-existent device
         if not self.device:
             self.dbg.stdout("Device not found! No preview.", self.dbg.error, 1)

commit 6565e8b59f87c6ca8dddde4801059899ff60aa17

controller: Fix regression when deleting items
Regressed from 44d4a0567120db9055b0101596baca92589e138b.
"self.feature" was removed in error. It shares code for file management
which will be used for the other tabs.

pylib/controller/effects.py
@@ -32,6 +32,7 @@ class EffectsTab(shared.CommonFileTab):
     """
     def __init__(self, appdata):
         super().__init__(appdata, effects.EffectFileManagement, "EffectContents", "EffectSidebarTree")
+        self.feature = "effects"
 
         # Populate tasks
         self.tasks = {

pylib/controller/presets.py
@@ -24,6 +24,7 @@ class PresetsTab(shared.TabData):
     """
     def __init__(self, appdata):
         super().__init__(appdata)
+        self.feature = "presets"
 
     def set_tab(self):
         """