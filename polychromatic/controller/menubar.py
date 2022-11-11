polychromatic/polychromatic

commit 1b473acddb0e71f7284c129c7610cc8965c86134

Evaluate $HOME instead of '/home/$USER'
For paths involving OpenRazer's config/logs, which wasn't respecting
that the home directory could be somewhere else.

Fixes #432.

polychromatic/controller/menubar.py
@@ -385,9 +385,9 @@ class MenuBarOpenRazer(MenuBar):
 
         if not cmd:
             try:
-                cmd = "xdg-open file:///home/{0}/.local/share/openrazer/logs/razer.log".format(os.environ["USER"])
+                cmd = "xdg-open file://{0}/.local/share/openrazer/logs/razer.log".format(os.environ["HOME"])
             except KeyError:
-                cmd = "xdg-open file:///home/$USER/.local/share/openrazer/logs/razer.log"
+                cmd = "xdg-open file:///home/$USER/.local/share/openrazer/logs/razer.log".format(os.environ["USER"])
 
         print("Running: " + cmd)
         os.system(cmd)

polychromatic/controller/preferences.py
@@ -295,7 +295,7 @@ class OpenRazerPreferences(shared.TabData):
 
         if not self.conf_path:
             try:
-                self.conf_path = "/home/{0}/.config/openrazer/razer.conf".format(os.environ["USER"])
+                self.conf_path = "{0}/.config/openrazer/razer.conf".format(os.environ["HOME"])
             except KeyError:
                 self.conf_path = "/home/$USER/.config/openrazer/razer.conf"
 

commit b9d50aa5459d637cfb70eb45fc7b95988e1b5973

CI: Fix too-many-format-args
Regressed from 1b473acddb0e71f7284c129c7610cc8965c86134

polychromatic/controller/menubar.py
@@ -387,7 +387,7 @@ class MenuBarOpenRazer(MenuBar):
             try:
                 cmd = "xdg-open file://{0}/.local/share/openrazer/logs/razer.log".format(os.environ["HOME"])
             except KeyError:
-                cmd = "xdg-open file:///home/$USER/.local/share/openrazer/logs/razer.log".format(os.environ["USER"])
+                cmd = "xdg-open file:///home/$USER/.local/share/openrazer/logs/razer.log"
 
         print("Running: " + cmd)
         os.system(cmd)