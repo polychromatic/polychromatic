bcada651f038254abbcad4a47b1fd0b3034ecbcd
Luke Horwell <code@horwell.me>
2024-04-22 22:14:07 +0100

    Remove broken 'Open Mouse Settings' option
    Technically this was fixable, since it just needed to read "id" from
    "device.form_factor". It's been broken and forgotten about for 2 years!
    
    In the interests of eliminating technical debt, it'll be removed since
    KDE 6 no longer opens this mouse page any more. It was originally added
    as a shortcut for the user to configure mouse settings (like
    acceleration) via their desktop environment.
    
    In future, we could bring it back with better version checks. Or, add a
    message to remind the user that they're worth checking out, since we
    can't directly read/set options.

--- polychromatic/controller/devices.py
+++ polychromatic/controller/devices.py
@@ -353,11 +353,6 @@ class DevicesTab(shared.TabData):
 
                 # TODO: Button to set compatible software effects - or as "Custom" effect?
 
-                # -- Mouse Acceleration
-                # TODO: Move to middleman for all interfaces?
-                if device.form_factor == "mouse":
-                    widgets.append(self.special_controls.create_mouse_accel_control())
-
                 # -- Information about OpenRazer daemon's built-in macro feature
                 if device.backend.backend_id == "openrazer" and device.form_factor.get("id") == "keyboard":
                     widgets.append(self.special_controls.create_openrazer_macro_control())
@@ -1282,74 +1277,6 @@ class SpecialControls(shared.TabData):
         _refresh_grid_size()
 
         return self.widgets.create_row_widget(self._("DPI"), [stage_widget, dpi_widget], vertical=True)
-
-    def create_mouse_accel_control(self):
-        """
-        Creates a button that'll either:
-          - Open mouse acceleration settings for known desktop environments
-          - Or inform the user if desktop environment unknown.
-
-        Due to the diverse range of desktop environments, getting mouse
-        acceleration values isn't supported right now.
-        """
-        # TODO: Move to middleman to use for all mice
-        def _get_current_desktop_env():
-            try:
-                desktop_env = os.environ["XDG_CURRENT_DESKTOP"]
-
-                # Some distros add a prefix/suffix ("ubuntu", "X-")
-                if desktop_env.find("GNOME") != -1:
-                    return "GNOME"
-                elif desktop_env.find("Cinnamon") != -1:
-                    return "Cinnamon"
-                elif desktop_env in ["KDE", "MATE", "Pantheon", "LXQt"]:
-                    return desktop_env
-                return "Unknown"
-            except KeyError:
-                return "Unknown"
-
-        def _get_command():
-            desktop_env = _get_current_desktop_env()
-            desktop_to_cmd = {
-                "GNOME": "gnome-control-center mouse",
-                "Cinnamon": "cinnamon-settings mouse",
-                "KDE": "systemsettings5 mouse",
-                "MATE": "mate-mouse-properties",
-                "Pantheon": "io.elementary.switchboard settings://input/mouse",
-                "LXQt": "lxqt-config-input",
-            }
-            try:
-                return desktop_to_cmd[desktop_env]
-            except KeyError:
-                return None
-
-        def _open_mouse_settings():
-            command = _get_command()
-            if command:
-                self.dbg.stdout("Opening: " + command, self.dbg.action, 1)
-                subprocess.Popen(command.split(" "))
-                return
-
-            # Desktop environment unknown, inform the user for manual action.
-            self.widgets.open_dialog(self.widgets.dialog_generic,
-                                     self._("Mouse Acceleration"),
-                                     self._("This feature is provided by your operating system. Polychromatic doesn't recognise this desktop environment " + \
-                                            "to automatically open the settings window for you.\n\n" + \
-                                            "Look for 'mouse', 'input' or 'hardware' in your System Settings."))
-
-        widget = QWidget()
-        layout = QHBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        widget.setLayout(layout)
-        button = QPushButton()
-        button.setText(self._("Open Mouse Settings"))
-        button.setIconSize(QSize(24, 24))
-        button.setIcon(QIcon.fromTheme("input-mouse"))
-        button.clicked.connect(_open_mouse_settings)
-        layout.addWidget(button)
-        layout.addStretch()
-
-        return self.widgets.create_row_widget(self._("Acceleration"), [widget])
 
     def create_programmable_keys_control(self, path):
         """
