e1ac15408f5d4e20e0b1b74ebc8e4d9bc151415a
Luke Horwell <code@horwell.me>
2025-03-15 19:35:58 +0000

    CI: Update requirements.txt

--- scripts/dev/update-requirements.sh
+++ scripts/dev/update-requirements.sh
@@ -1,7 +1,17 @@
-#!/bin/bash -x
-
+#!/bin/bash -xe
+#
+# Updates the repository's requirements.txt file, which is for use with
+# the CI's virtual environment.
+#
 cd "$(dirname "$0")/../../"
-python -m venv /tmp/venv
-source /tmp/venv/bin/activate
+
+TEMP_DIR="$(mktemp -d)"
+
+python -m venv "${TEMP_DIR}"
+source "${TEMP_DIR}/bin/activate"
+
 pip install pip-tools
 pip-compile -U
+
+git add requirements.txt
+git commit -m "CI: Update requirements.txt"
