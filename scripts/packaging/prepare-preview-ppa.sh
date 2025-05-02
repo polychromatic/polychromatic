a67968aff35d31a37829d42506d3b7995d6230dc
Luke Horwell <code@horwell.me>
2025-05-02 15:13:50 +0100

    CI: Add 25.10 to preview PPA; drop 24.10

--- scripts/packaging/prepare-preview-ppa.sh
+++ scripts/packaging/prepare-preview-ppa.sh
@@ -24,5 +24,5 @@ function process_release() {
 }
 
 process_release "24.04" "noble"     # LTS
-process_release "24.10" "oracular"
 process_release "25.04" "plucky"
+process_release "25.10" "questing"
